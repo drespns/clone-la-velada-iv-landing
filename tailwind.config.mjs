b37809347aae63367ba28126d4ddf031b12a505a
Daniel Rguez Espinosa <drespns@gmail.com>
2024-09-29 18:16:43 +0100

    THREE.JS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 import animations from '@midudev/tailwind-animations';
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class', // Habilita el modo oscuro usando la clase "dark"
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
